b47eb6039340b32e9468e18b015381a5a131fe9b
run-after <run.after@protonmail.com>
2020-05-12 09:07:39 -0500

    Finish first draft of User

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    if @user
+      session[:id] = @user.id
+      redirect_to user_path(@user) ### NOT WORKING
+    else
+      flash[:danger] = "Username not found"
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'sessions/new'
+  get 'sessions/create'
+resources :users#, only:[:new, :create, :show]
+
+get 'sign_in', to: 'sessions#new'
+post 'sign_in', to: 'sessions#create'
+delete 'sign_out', to: 'sessions#destroy'
+
+root 'sessions#new'
+
 end
